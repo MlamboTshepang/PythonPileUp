fffdf7cfd026395c0c1a9014bc559c99e1360e65
MlamboTshepang <Tshepangmlambo@gmail.com>
2026-03-10 08:30:24 +0200

    Distinct

---
+++ Codility/ Distinct.py
@@ -0,0 +1,14 @@
+def main():
+    Array = [2,1,1,2,3,1]
+    for i in range(len(Array)):
+        if (i not in array):
+            print({i} + "is not in the array")
+        else:
+            print(i)
+
+
+
+if __name__ == "__main__":
+    main()
+
+# Mlambo Curations 2026 - Codility Distinct Test Attempt
